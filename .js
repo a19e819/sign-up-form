fd4dbe8e68c6d300d42d0cbcf59c45ace6be17c4
a19e819 <ainfrod0411@gmail.com>
2025-04-14 00:15:06 +0200

    sign-up form-box transition

--- .js
+++ .js
@@ -1,5 +1,22 @@
+// submit button color transition
 const input = document.getElementById("email");
 
 input.addEventListener("focus", () => {
     input.style.transition = "border-color 0.5s ease, opacity 0.5s ease";
 });
+
+// sign up/login form-boxes transition
+const login = document.querySelector('.form-box-login');
+const signup = document.querySelector('.form-box-sign-up');
+const loginLink = document.querySelector('.login');
+const signupLink = document.querySelector('.sign-up');
+
+signupLink.addEventListener('click', () => {
+    signup.classList.add('active');
+    login.classList.add('active');
+});
+
+loginLink.addEventListener('click', () => {
+    signup.classList.remove('active');
+});
+
